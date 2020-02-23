4405776d4f4a7a9b7f1bb66e9941d8b111cd7004
dcastelar78byui <dcastelar78@byui.edu>
2020-02-23 00:07:07 -0300

    initial commit

--- assignments/lesson7/js/lazyloading.js
+++ assignments/lesson7/js/lazyloading.js
@@ -1,34 +1,28 @@
-const image = document.querySelectorAll('[data-src]');
-
-const imagesToLoad= document.querySelectorAll('img[data-src]');
-
-
-const imgOptions= {
-    threshold: 0,
-    rootMargin: "0px 0px -300px 0px",
+const images = document.querySelectorAll("[data-src]");
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 300px 0px"
 };
+const imgObserver = new IntersectionObserver((entries,
+                                              imgObserver) =>{
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        }else{
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {image.removeAttribute('data-src');};
-};
-
-//first check to see if Intersection Observer is supported
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer)=> {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
-    });
-    imagesToLoad.forEach((img) => {
-        observer.observe(img);
-    });
-} else {
-    imagesToLoad.forEach((img)=> {
-        loadImages(img);
-    });
-        
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
 }
+
+images.forEach(image => {
+   imgObserver.observe(image)
+});
